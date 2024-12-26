9461fe7baddde31cbb639dcefeba507ed9ccc314
maz12340 <163176767+maz12340@users.noreply.github.com>
2024-12-27 00:43:40 +0300

    Create RoleService.java

---
+++ src/main/java/org/example/pharm/service/RoleService.java
@@ -0,0 +1,27 @@
+package org.example.pharm.service;
+
+import org.example.pharm.model.Role;
+import org.example.pharm.repository.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Сервис для работы с ролями пользователей.
+ * Предоставляет методы для получения информации о ролях.
+ */
+@Service
+public class RoleService {
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    /**
+     * Получает роль по идентификатору.
+     *
+     * @param id Идентификатор роли.
+     * @return Роль с заданным идентификатором.
+     */
+    public Role get(Long id) {
+        return roleRepository.findById(id).get();
+    }
+}
