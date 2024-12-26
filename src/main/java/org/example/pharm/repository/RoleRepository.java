c250ad87130715d133886d6454997bfb10e42e98
maz12340 <163176767+maz12340@users.noreply.github.com>
2024-12-27 00:41:59 +0300

    Create RoleRepository.java

---
+++ src/main/java/org/example/pharm/repository/RoleRepository.java
@@ -0,0 +1,27 @@
+package org.example.pharm.repository;
+
+import org.example.pharm.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+/**
+ * Репозиторий для работы с {@link Role}.
+ * Обеспечивает доступ к данным ролей и предоставляет методы для поиска роли по имени.
+ */
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    /**
+     * Ищет роль по имени.
+     *
+     * @param name Имя роли.
+     * @return Опционально возвращает роль с заданным именем.
+     */
+    Optional<Role> findByName(String name);
+}
+
+
+
+
